e7aacff539ce87bccbb9f2d1fd76a85446a263a7
Jenish <63183649+jenishborah@users.noreply.github.com>
2022-12-17 16:58:02 +0530

    Add files via upload

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,43 +1,38 @@
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
- addEventListener("click", () => {
+addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 
 window.addEventListener("click", () => {
-    if(document.querySelector(".style-switcher").classList.contains("open"))
-    {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
     }
 })
 
 const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color)
-{
- this.alternateStyles.forEach((style) => {
-if(color === style.getAttribute("title"))
-{
-    style.removeAttribute("disabled")
-} 
-else
-{
-    style.setAttribute("disabled","false")
-}
-})
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled")
+        }
+        else {
+            style.setAttribute("disabled", "false")
+        }
+    })
 }
 
 const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () =>{
+dayNight.addEventListener("click", () => {
     document.querySelector("i").classList.toggle("fa-sun");
     document.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
 })
 window.addEventListener("load", () => {
-if(document.body.classList.contains("dark"))
-{
-    document.querySelector("i").classList.add("fa-sun");
-}
-else{
-    document.querySelector("i").classList.add("fa-moon");
-}
+    if (document.body.classList.contains("dark")) {
+        document.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        document.querySelector("i").classList.add("fa-moon");
+    }
 
 })
